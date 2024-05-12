f7b3e13b9fafd24013f92499945e4844da1365ba
vladimir713 <vladimir713@mail.ru>
2024-05-12 22:55:29 +0300

    Добавил кнопку выбора файла

--- src/main/java/com/example/sync/HelloController.java
+++ src/main/java/com/example/sync/HelloController.java
@@ -1,14 +1,45 @@
 package com.example.sync;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Stage stage;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    final FileChooser fileChooser = new FileChooser();
+    private Desktop desktop = Desktop.getDesktop();
+
+    Logger LOG = Logger.getLogger(HelloController.class.getName());
+    @FXML
+    protected void onOpenFileButton() {
+        File file = fileChooser.showOpenDialog(stage);
+        if (file != null) {
+            openFile(file);
+        }
+    }
+
+    private void openFile(File file) {
+        try {
+            desktop.open(file);
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, "Ошибка ввода-вывода", ex);
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.sync {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires java.logging;
 
 
     opens com.example.sync to javafx.fxml;
