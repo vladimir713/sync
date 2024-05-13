690ed06c04d2406ea31afa5a36b2c6e3bd5d1090
vladimir713 <vladimir713@mail.ru>
2024-05-13 23:04:37 +0300

    Добавил текстовое поле с выводом полного пути к открываемому файлу

--- src/main/java/com/example/sync/HelloController.java
+++ src/main/java/com/example/sync/HelloController.java
@@ -1,15 +1,11 @@
 package com.example.sync;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
-import java.awt.Desktop;
+import javafx.scene.control.TextField;
 import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class HelloController {
@@ -17,14 +13,14 @@ public class HelloController {
     private Label welcomeText;
     @FXML
     private Stage stage;
-
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
-
+    @FXML
+    protected TextField path;
     final FileChooser fileChooser = new FileChooser();
-    private Desktop desktop = Desktop.getDesktop();
+//    private Desktop desktop = Desktop.getDesktop();
 
     Logger LOG = Logger.getLogger(HelloController.class.getName());
     @FXML
@@ -36,10 +32,7 @@ public class HelloController {
     }
 
     private void openFile(File file) {
-        try {
-            desktop.open(file);
-        } catch (IOException ex) {
-            LOG.log(Level.SEVERE, "Ошибка ввода-вывода", ex);
-        }
+        path.setText(file.getAbsolutePath());
+//            desktop.open(file);
     }
 }
